a28d7b37699e51cceca1f75e69e6d2e623ab69ed
Takeshi Ueda <t.ueda@0004s.com>
2014-10-27 17:03:22 +0900

    Add route setting

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,37 +1,28 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+	subject { page }
 	describe "Home Page" do
-		it "should have the content 'Sample App'" do
-			visit '/static_pages/home'
-			expect(page).to have_content('Sample App')
-		end
-
-		it "should have the right title" do
-			visit '/static_pages/home'
-			expect(page).to have_title('Rails app | Home')
-		end
+		before { visit root_path }
+		it {should have_content('Sample App')}
+		it {should have_title('Rails app | Home')}
 	end
 
 	describe "Help page" do
-		it "should have the content 'Help'" do
-			visit '/static_pages/help'
-			expect(page).to have_content('Help')
-		end
-		it "should have the right title" do
-			visit '/static_pages/help'
-			expect(page).to have_title('Rails app | Help')
-		end
+		before { visit help_path }
+		it {should have_content('Help')}
+		it {should have_title('Rails app | Help')}
 	end
 
 	describe "About page" do
-		it "should have the content 'About Us'" do
-			visit '/static_pages/about'
-			expect(page).to have_content('About Us')
-		end
-		it "should have the right title" do
-			visit '/static_pages/about'
-			expect(page).to have_title('Rails app | About')
-		end
+		before { visit about_path }
+		it {should have_content('About Us')}
+		it {should have_title('Rails app | About')}
+	end
+
+	describe "Contact page" do
+		before { visit contact_path }
+		it {should have_content('Contact')}
+		it {should have_title('Rails app | Contact')}
 	end
 end
